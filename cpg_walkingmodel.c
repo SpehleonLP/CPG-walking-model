685bb6b1794c7715e84df02e0603eeb5494b21ce
Spehleon LP <61126480+SpehleonLP@users.noreply.github.com>
2023-07-28 17:55:07 -0500

    Update cpg_walkingmodel.c

--- cpg_walkingmodel.c
+++ cpg_walkingmodel.c
@@ -6,7 +6,7 @@
 #include <stddef.h>
 #include <stdlib.h>
 
-void CPG_Update(CPG_constants const* constants, CPG_neuron * neurons, int size, CPG_senses const* senses, int senses_size);
+void CPG_Update(CPG_Model * model, float dt);
 
 struct CPG_neuron
 {
@@ -17,12 +17,12 @@ struct CPG_neuron
 
 struct CPG_Model * CPG_ModelCreate(int noSegments)
 {
-	int noSenses     = noSegments*2;
-	int noNeurons    = noSenses*2;
+	int noLegs     = noSegments*2;
+	int noNeurons  = noLegs*2;
 
 	size_t size = sizeof(struct CPG_Model) 
 				+ sizeof(CPG_neuron) * noNeurons
-				+ sizeof(CPG_senses) * noSenses
+				+ sizeof(float) * noLegs
 				+ sizeof(CPG_Segment) * noSegments
 				+ sizeof(char) * noSegments
 				+ 16;
@@ -30,8 +30,13 @@ struct CPG_Model * CPG_ModelCreate(int noSegments)
 	
 	struct CPG_Model * r = calloc(size, 1);
 		
-	r->settings.state_memory_constant		= 0.0473;	
-	r->settings.fatigue_memory_constant		= 0.6; 
+//	r->settings.state_memory_constant		= 0.0473;	
+//	r->settings.fatigue_memory_constant		= 0.6; 
+//	r->settings.brain_signal_strength		= 1.71;
+	
+	r->settings.state_memory_half_life		= CPG_ComputeHalfLife(0.0473, 0.1);		
+	r->settings.fatigue_memory_half_life	= CPG_ComputeHalfLife(0.6, 0.1);	
+	
 	r->settings.brain_signal_strength		= 1.71;
 	
 	r->settings.recurrent_inhibition		= 3.0;	
@@ -51,23 +56,31 @@ struct CPG_Model * CPG_ModelCreate(int noSegments)
 	r->drivers.unit[PD_Stance][PD_Hip].derivative	= 1;
 	
 	r->drivers.unit[PD_Swing][PD_Knee].target		= 0.57;	
-	r->drivers.unit[PD_Swing][PD_Knee].proportional = 1.86;
-	r->drivers.unit[PD_Swing][PD_Knee].derivative	= 2;
+	r->drivers.unit[PD_Swing][PD_Knee].proportional = 1.860;
+	r->drivers.unit[PD_Swing][PD_Knee].derivative	= 0.40;
 	
 	r->drivers.unit[PD_Stance][PD_Knee].target		= 1.30;	
-	r->drivers.unit[PD_Stance][PD_Knee].proportional= 1.97;
-	r->drivers.unit[PD_Stance][PD_Knee].derivative	= 2;
+	r->drivers.unit[PD_Stance][PD_Knee].proportional= 1.970;
+	r->drivers.unit[PD_Stance][PD_Knee].derivative	= 0.40;
 	
 	*(int*)&r->noSegments = noSegments;	
 	*(int*)&r->noNeurons = noNeurons;
-	*(int*)&r->noSenses = noSenses;
+	*(int*)&r->noLegs = noLegs;
 	
 	*(void**)&r->neurons = r+1;	
-	*(void**)&r->senses = r->neurons + noNeurons;
-	*(void**)&r->segments = r->senses + noSenses;
+	*(void**)&r->contact_force = r->neurons + noNeurons;
+	*(void**)&r->segments = r->contact_force + noLegs;
 	*(void**)&r->legState = r->segments + noSegments;
 	
-	assert(r->legState + (noSenses) < (uint8_t*)r + size);
+	
+/*	struct CPG_Leg * legs = &r->segments->leg[0];
+	for(int i = 0; i < noLegs; ++i)
+	{
+		legs[i].knee.pos = r->drivers.unit[PD_Stance][PD_Knee].target;
+	}
+	
+	*/
+	assert((uint8_t*)r->legState + (noLegs) < (uint8_t*)r + size);
 	
 	fclose(fopen("cpg_test_data.txt", "w"));
 	
@@ -75,56 +88,94 @@ struct CPG_Model * CPG_ModelCreate(int noSegments)
 }
 
 void CPG_ModelUpdate(CPG_Model * model, float dt)
-{	
+{		
 	int noLegs =  model->noSegments*2;
 	struct CPG_Leg * legs = &model->segments->leg[0];
 
-	if((model->biotick += dt) > 0.1)
+	float tick_rate = 0.5;
+#if 1
+	if((model->biotick += dt) > tick_rate)
 	{
-		for(int i = 0; i < noLegs; ++i)
-			model->senses[i].hip_theta = legs[i].hip.pos;	
-		
 		do
 		{
-			CPG_Update(&model->settings, model->neurons, model->noNeurons, model->senses, model->noSenses);
-		} while((model->biotick -= 0.1) > 0.1); 
-		
+			CPG_Update(model, tick_rate);
+		} while((model->biotick -= tick_rate) > tick_rate); 
+#if 1
 		for(int i = 0; i < noLegs; ++i)
 		{
 			float extensor = model->neurons[i*2 + 0].output;
 			float flexor   = model->neurons[i*2 + 1].output;
-			
-			int p = model->legState[i];
-			
+						
 			if(extensor != flexor)
-				model->legState[i] = extensor > flexor;
-			
-			if(p != model->legState[i] && i == 0)
 			{
-				fprintf(stderr, "switch!");	
+				model->legState[i] = extensor > flexor;
 			}
 		}
+#endif
+#if 0
+		FILE *fp = fopen("cpg_test_data.txt", "a");
+		
+		for(int i = 0; i < noLegs; ++i)
+		{
+			fprintf(fp, "%c ", model->legState[i]? 'x' : ' ');
+		}
+		
+		fprintf(fp, "\n");
+		fclose(fp);
+#endif
 	}
+#else
 	
-	dt *= 0.5;
+	CPG_Update(&model->settings, model->neurons, model->noNeurons, model->senses, model->noSenses, dt);
+	
+#endif
+
+	
+	float invDt = dt? 1.f / dt : 1.f;
+	float halfDt = dt*0.5;
+	
+//FILE *fp = fopen("cpg_test_data.txt", "a");
+//	
+//	fprintf(fp, "%f\t", total_time);
+
 	
 	for(int i = 0; i < noLegs; ++i)
 	{
 		int s = model->legState[i];		
 		
-		float torque = model->drivers.unit[s][PD_Hip].proportional * (model->drivers.unit[s][PD_Hip].target - legs[i].hip.pos) - model->drivers.unit[s][PD_Hip].derivative * legs[i].hip.velo;		
-		float accel = model->drivers.unit[s][PD_Knee].proportional * (model->drivers.unit[s][PD_Knee].target - legs[i].knee.pos) - model->drivers.unit[s][PD_Knee].derivative * legs[i].knee.velo;
+		float hip_error = model->drivers.unit[s][PD_Hip].target	  - legs[i].hip.pos;
+		float knee_error = model->drivers.unit[s][PD_Knee].target - legs[i].knee.pos;
 		
-		legs[i].hip.velo += torque * dt;		
-		legs[i].hip.pos += legs[i].hip.velo * dt;		
-		legs[i].hip.velo += torque * dt;		
+		float p_hip_error = model->drivers.unit[s][PD_Hip].target	- legs[i].hip.p_pos;
+		float p_knee_error = model->drivers.unit[s][PD_Knee].target - legs[i].knee.p_pos;
 		
-		legs[i].knee.velo += accel * dt;		
-		legs[i].knee.pos  += legs[i].knee.velo * dt;		
-		legs[i].knee.velo += accel * dt;	
+		float d_hip_error  = (hip_error - p_hip_error) * invDt;		
+		float d_knee_error = (knee_error - p_knee_error) * invDt;
 		
-		assert(&legs[i] < &model->legState[0]);
+		float hip_pd = model->drivers.unit[s][PD_Hip].proportional   * (hip_error  + model->drivers.unit[s][PD_Hip].derivative * d_hip_error);		
+		float knee_pd = model->drivers.unit[s][PD_Knee].proportional * (knee_error + model->drivers.unit[s][PD_Knee].derivative * d_knee_error);
+		
+		legs[i].hip.p_pos = legs[i].hip.pos;		
+		legs[i].knee.p_pos = legs[i].knee.pos;
+		
+		legs[i].hip.velocity += hip_pd * halfDt;		
+		legs[i].knee.velocity += knee_pd * halfDt;	
+		
+		legs[i].hip.pos  += legs[i].hip.velocity * dt;
+		legs[i].knee.pos += legs[i].knee.velocity * dt;	
+		
+		legs[i].hip.velocity += hip_pd * halfDt;		
+		legs[i].knee.velocity += knee_pd * halfDt;	
+		
+	//	if(i == 1)
+	//	fprintf(fp, "%f\t%f\t%f\t%f\t%f", legs[i].hip.pos, legs[i].hip.velocity, hip_error, d_hip_error, hip_pd);
+		
+		assert((void*)&legs[i] < (void*)&model->legState[0]);
 	}
+	
+//	fprintf(fp, "\n");
+	//fclose(fp);
+	
 }
 
 void PD_ModelSpline(PD_Model * dst, PD_Model const* src, float t)
@@ -171,90 +222,108 @@ void PD_ModelLerp(PD_Model * dst, PD_Model const* src0, PD_Model const* src1, fl
 	}
 }
 
-void CPG_Update(CPG_constants const* constants, CPG_neuron * neurons, int size, CPG_senses const* senses, int senses_size)
+void CPG_Update(CPG_Model * model, float dt)
 {
-	assert(size % 4 == 0);
-// operate on pairs of legs each pair has 2 neurons
-	assert(senses_size = size / 2);
+	CPG_constants const* constants = &model->settings;
+	CPG_neuron * neurons = model->neurons;
+	float * weight_on_foot = model->contact_force;
 	
-	int noSegments = size / 4;
+	struct CPG_Leg * legs = &model->segments->leg[0];
+	
+	static float total_time = 0;
+	total_time += dt;
+		
+	float state_memory_constant = pow(0.5, dt / constants->state_memory_half_life);	
+	float fatigue_memory_constant = pow( 0.5,  dt / constants->fatigue_memory_half_life);
+	
+//	float state_memory_constant = constants->state_memory_half_life;	
+//	float fatigue_memory_constant = constants->fatigue_memory_half_life;
+	
+	for(int i = 0; i < model->noNeurons; i += 2)
+	{
+		int segment = i / 4;
+		int side    = (i & 0x02);
+		int opposite_neuron = side? i - 2 : i + 2;
+		
+		float inhibition = 0;
+		float excitation = 0; 
+		
+		// extension and flexation inhibit each other within the same CPG unit
+		inhibition = constants->sensory_inhibition * neurons[i+1].output;		
+		
+		// flexation laterally inhibits flexation		
+		inhibition += constants->contralateral_inhibition * neurons[opposite_neuron].output;
+		 
+		if(segment != 0)
+		{
+			inhibition += constants->ipsilateral_inhibition * neurons[i-4].output;
+		}
+		if(segment < model->noSegments)
+		{
+			inhibition += constants->ipsilateral_inhibition * neurons[i+4].output;
+		}
+		
+		inhibition += constants->recurrent_inhibition * neurons[i].fatigue;
+		
+		excitation = constants->brain_signal_strength;
+		
+		excitation += legs[i/2].hip.pos * constants->hip_feedback_constant;
+		float state = state_memory_constant * neurons[i].state + (excitation - inhibition);
+		
+		neurons[i].state 	= state;
+		neurons[i].fatigue  = fatigue_memory_constant * neurons[i].fatigue + neurons[i].output;
+		neurons[i].output   = state * (state > 0);
+	}
+	
+	for(int i = 1; i < model->noNeurons; i += 2)
+	{
+		int segment = i / 4;
+		int side    = (i & 0x02);
+		int opposite_neuron = side? i - 2 : i + 2;
+		
+		float inhibition = 0;
+		float excitation = 0; 
+		
+		// extension and flexation inhibit each other within the same CPG unit
+		inhibition = constants->sensory_inhibition * neurons[i-1].output;		
+		
+		// flexation laterally inhibits flexation		
+		inhibition += constants->contralateral_inhibition * neurons[opposite_neuron].output;
+		 
+		if(segment != 0)
+		{
+			inhibition += constants->ipsilateral_inhibition * neurons[i-4].output;
+		}
+		if(segment < model->noSegments)
+		{
+			inhibition += constants->ipsilateral_inhibition * neurons[i+4].output;
+		}
+		
+		inhibition += constants->recurrent_inhibition * neurons[i].fatigue;
+		excitation = constants->brain_signal_strength;
+		
+		inhibition += legs[i/2].hip.pos *-constants->hip_feedback_constant;
+		excitation += weight_on_foot[i/2] * constants->foot_feedback_constant;
+		
+		float state = state_memory_constant * neurons[i].state + (excitation - inhibition);
+		
+		neurons[i].state 	= state;
+		neurons[i].fatigue  = fatigue_memory_constant * neurons[i].fatigue + neurons[i].output;
+		neurons[i].output   = state * (state > 0);
+	}
+#if 0
+	FILE *fp = fopen("cpg_test_data.txt", "a");
+	
+	fprintf(fp, "%f", total_time);
 	
 	for(int i = 0; i < size; i += 2)
 	{
-		int segment = i / 4;
-		int side    = (i & 0x02);
-		int opposite_neuron = side? i - 2 : i + 2;
-		
-		float		
-		// extension and flexation inhibit each other within the same CPG unit
-		state = constants->sensory_inhibition * neurons[i+1].output;		
-		
-		// flexation laterally inhibits flexation		
-		state += constants->contralateral_inhibition * neurons[opposite_neuron].output;
-		 
-		if(segment != 0)
-		{
-			state += constants->ipsilateral_inhibition * neurons[i-4].output;
-		}
-		if(segment < noSegments)
-		{
-			state += constants->ipsilateral_inhibition * neurons[i+4].output;
-		}
-		
-		state += constants->recurrent_inhibition * neurons[i].fatigue;
-		state = constants->brain_signal_strength - state;
-		
-		state += senses[i/2].hip_theta * constants->hip_feedback_constant;
-		state = state + constants->state_memory_constant * neurons[i].state;
-		
-		neurons[i].state 	= state;
-		neurons[i].fatigue  = neurons[i].output + constants->fatigue_memory_constant * neurons[i].fatigue;
-		neurons[i].output   = state * (state > 0);
+		fprintf(fp, "\t%f", neurons[i].output - neurons[i+1].output);
 	}
 	
-	for(int i = 1; i < size; i += 2)
-	{
-		int segment = i / 4;
-		int side    = (i & 0x02);
-		int opposite_neuron = side? i - 2 : i + 2;
-		
-		float		
-		// extension and flexation inhibit each other within the same CPG unit
-		state = constants->sensory_inhibition * neurons[i-1].output;		
-		
-		// flexation laterally inhibits flexation		
-		state += constants->contralateral_inhibition * neurons[opposite_neuron].output;
-		 
-		if(segment != 0)
-		{
-			state += constants->ipsilateral_inhibition * neurons[i-4].output;
-		}
-		if(segment < noSegments)
-		{
-			state += constants->ipsilateral_inhibition * neurons[i+4].output;
-		}
-		
-		state += constants->recurrent_inhibition * neurons[i].fatigue;
-		state = constants->brain_signal_strength - state;
-		
-		state += senses[i/2].hip_theta *-constants->hip_feedback_constant + senses[i/2].weight_on_foot * constants->foot_feedback_constant;
-		state = state + constants->state_memory_constant * neurons[i].state;
-		
-		neurons[i].state 	= state;
-		neurons[i].fatigue  = neurons[i].output + constants->fatigue_memory_constant * neurons[i].fatigue;
-		neurons[i].output   = state * (state > 0);
-	}
-	
-	FILE *fp = fopen("cpg_test_data.txt", "a");
-	
-	for(int i = 0; i < size; ++i)
-	{
-		fprintf(fp, "%f\t", neurons[i].output);
-	}
-	
-	fprintf(fp, "\n");
+	fprintf(fp, "\t");
 	fclose(fp);
-	
+#endif
 	/*
 	for(int i = 0; i < size; ++i)
 	{		
@@ -293,3 +362,30 @@ void CPG_Update(CPG_constants const* constants, CPG_neuron * neurons, int size, 
 	};	*/
 }
 
+
+float CPG_GetCenterOfGravity(float * dst, float * xyz, float * mass, int size)
+{
+	float accumulator[3] = {0, 0, 0};
+	float total_mass = 0;
+	
+	for(int i = 0; i < size; ++i)
+	{
+		accumulator[0] += xyz[i*3+0] * mass[i];
+		accumulator[1] += xyz[i*3+1] * mass[i];
+		accumulator[2] += xyz[i*3+2] * mass[i];
+		total_mass += mass[i];
+	}
+	
+	float invMass = total_mass? 1.f / total_mass : 1.f;
+	
+	dst[0] = accumulator[0] * invMass;	
+	dst[1] = accumulator[1] * invMass;	
+	dst[2] = accumulator[2] * invMass;
+		
+	return total_mass;
+}
+
+float CPG_ComputeHalfLife(float percent_remaining, float time)
+{
+	return time / log2(percent_remaining) / -1.f;
+}
